70d24375830a730109443d7054678d19721076ba
jotham Soboyejo <jothamsoboyejo@Rotimis-Mac-mini-2.local>
2021-07-25 16:40:54 +0100

    Covered Booleans in dart

---
+++ 4.Booleans.dart
@@ -0,0 +1,24 @@
+void main()
+{
+    // BOOLEANS
+    //---------
+
+    // To represent boolean values, dart has a type name 'bool'. Only two object have bool: the booleans literals true and
+    // false, which are bothe compile-time constants.
+
+    // Check for an empty string
+    var fullName = '';
+    assert(fullName.isEmpty);
+
+    // Check for zero.
+    var hitPoints = 0;
+    assert(hitPoints <= 0);
+
+    // check for null
+    var unicorn;
+    assert(unicorn == null);
+
+    // Check for NaN
+    var iMeantToDoThis = 0 / 0;
+    assert(iMeantToDoThis.isNaN);
+}
